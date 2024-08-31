1416be7bab066119f0c938fef5cb7815491c9329
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-08-31 22:18:28 +0530

    Update Mobile.jsx

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 
 
 
-
 export default function Mobile() {
   return (
     <div className='w-full min-h-[70vh] bg-gradient-to-tr from-fuchsia-200 to-pink-50 flex items-center justify-center'>
